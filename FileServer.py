98ec4b48cf8f3a1a185de90b18370e9090240084
Kim Lindhard <kim.lindhard@gmail.com>
2013-05-02 10:51:09 +0200

    version of FIleServer not used

--- FileServer.py
+++ FileServer.py
@@ -1,45 +1,55 @@
-
+import select
 import socket
 import threading
-
+import sys
 class FileServer(threading.Thread):
     def __init__(self,adress="", port=5001):
         threading.Thread.__init__(self)
         self.adress = adress
         self.port = port
-        self.file = None
+        #self.file = None
         self.fileQue = []
+        self.shouldRun = True
         self.stayOpen = True
         self.start()
         
-    def serveFile(self, file):
-        self.fileQue.append(file)
+    def serveFile(self, inFile):
         
+        self.stayOpen = True
+        self.fileQue.append(inFile)
+        
+    def closeConnection(self):
+        self.stayOpen = False
     def stop(self):
-        try:
-            self.mySocket.close()
-            self.myConnection.close()
-        except:
-            pass
+        self.stayOpen = False
+        self.shouldRun = False
         
-    def run(self):
-        while self.stayOpen:
-            if len(self.fileQue) > 0:
-                self.mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                self.mySocket.bind((self.adress, self.port))
-                self.mySocket.listen(1)
-                filePath =  self.fileQue.pop()
-                rawFile = open(filePath, "rb")
-                data = filePath + "#"
-                data += rawFile.read()
-                rawFile.close()
-                self.notify("status", "Ready to serve filePath:" + filePath)
-                self.myConnection, maddr = self.mySocket.accept()
-                self.mySocket.close()
-                self.myConnection.send(data)
-                self.myConnection.close()
-                self.notify("status", "File sent:" + filePath)
+    def run(self): # SEMI TESTED
+        self.mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.mySocket.bind((self.adress, self.port))
+        self.mySocket.listen(1)
+        
+        while self.shouldRun:   
+            while self.stayOpen:
+                if len(self.fileQue) > 0:
+                    self.notify("status", "Ready to serve file:" + "filePath")
+                    readable, writable, exceptional = select.select([self.mySocket], [], [], 3)
+                    for s in readable:
+                        if s is self.mySocket:
+                            self.myConnection, address = self.mySocket.accept()
+                            #print "Connection from", address
+                            filePath =  self.fileQue.pop()
+                            rawFile = open(filePath, "rb")
+                            data = filePath + "#"
+                            data += rawFile.read()
+                            rawFile.close()
+                            self.myConnection.send(data)
+                            self.myConnection.close()
+                            self.notify("status", "File sent:" + filePath)
+                #print "I am open and waiting"
+            #print "I closed"
+            self.fileQue = []
             
     observers = []
     SUBJECT_NAME = "FileServer"
